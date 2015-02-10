cde849df0ec8c52b093f4fd3468d7e41c50a0fdb
jeldrik <jeldriks@gmail.com>
2015-02-10 13:45:43 +0100

    Testing inner join query on database with rawquery()

--- app/src/main/java/app/com/jeldrik/teacherslittlehelper/MainActivity.java
+++ app/src/main/java/app/com/jeldrik/teacherslittlehelper/MainActivity.java
@@ -1,6 +1,7 @@
 package app.com.jeldrik.teacherslittlehelper;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.v4.app.FragmentTransaction;
@@ -44,32 +45,22 @@ public class MainActivity extends ActionBarActivity implements NewClassFragment.
                     .add(R.id.mainFragment, mainFragment)
                     .commit();
         }
-        SQLiteDatabase myDataBase=new DbHelper(this).getWritableDatabase();
+        SQLiteDatabase myDataBase=new DbHelper(this).getReadableDatabase();
 
-        ContentValues vals=new ContentValues(7);
-
-        vals.put(DbContract.ClassEntry.COLUMN_TITLE,"the Title");
-        vals.put(DbContract.ClassEntry.COLUMN_TIME,"17:00");
-        vals.put(DbContract.ClassEntry.COLUMN_DATE,"21.02.2015");
-        vals.put(DbContract.ClassEntry.COLUMN_DURATION, 90);
-        vals.put(DbContract.ClassEntry.COLUMN_LOCATION,"Madrid");
-        vals.put(DbContract.ClassEntry.COLUMN_LEVEL,"A1");
-        vals.put(DbContract.ClassEntry.COLUMN_EXTRA_INFO,"no extra info");
-        myDataBase.insert(DbContract.ClassEntry.TABLE_NAME,null,vals);
-        
-        vals=new ContentValues(4);
-        vals.put(DbContract.ClassContentEntry.COLUMN_BOOK, "no Book");
-        vals.put(DbContract.ClassContentEntry.COLUMN_PAGE, "15-20");
-        vals.put(DbContract.ClassContentEntry.COLUMN_INFO, "no info");
-        vals.put(DbContract.ClassContentEntry.COLUMN_FOREIGN_KEY_CLASS, 1);
-        myDataBase.insert(DbContract.ClassContentEntry.TABLE_NAME,null,vals);
-
-        vals=new ContentValues(4);
-        vals.put(DbContract.StudentEntry.COLUMN_STUDENT_NAME, "Petro");
-        vals.put(DbContract.StudentEntry.COLUMN_EMAIL, "petro@email.com");
-        vals.put(DbContract.StudentEntry.COLUMN_PHONE, "9111234532");
-        vals.put(DbContract.StudentEntry.COLUMN_FOREIGN_KEY_CLASS, 1);
-        myDataBase.insert(DbContract.StudentEntry.TABLE_NAME,null,vals);
+      /*  Cursor cursor=myDataBase.query(DbContract.ClassEntry.TABLE_NAME,new String[]{DbContract.ClassEntry.COLUMN_TITLE,DbContract.ClassEntry.COLUMN_LOCATION},null,null,null,null,null);
+        cursor.moveToFirst();
+        while(!cursor.isAfterLast()){
+            Log.v("MYCURSOR",cursor.getString(0)+" location: "+cursor.getString(1));
+            cursor.moveToNext();
+        }*/
+        String q="Select student.studentName,class.title from student inner join class on student.classID=class._id;";
+        Cursor cursor=myDataBase.rawQuery(q,null);
+        if(cursor.moveToFirst()) {
+            while (!cursor.isAfterLast()) {
+                Log.v("MYCURSOR", cursor.getString(0) + " location: " + cursor.getString(1));
+                cursor.moveToNext();
+            }
+        }
 
         myDataBase.close();
     }
--- app/src/main/java/app/com/jeldrik/teacherslittlehelper/data/ClassContentProvider.java
+++ app/src/main/java/app/com/jeldrik/teacherslittlehelper/data/ClassContentProvider.java
@@ -29,6 +29,32 @@ public class ClassContentProvider extends ContentProvider {
 
     @Override
     public Uri insert(Uri uri, ContentValues values) {
+
+        ContentValues vals=new ContentValues(7);
+/*
+        vals.put(DbContract.ClassEntry.COLUMN_TITLE,"the Title");
+        vals.put(DbContract.ClassEntry.COLUMN_TIME,"17:00");
+        vals.put(DbContract.ClassEntry.COLUMN_DATE,"21.02.2015");
+        vals.put(DbContract.ClassEntry.COLUMN_DURATION, 90);
+        vals.put(DbContract.ClassEntry.COLUMN_LOCATION,"Madrid");
+        vals.put(DbContract.ClassEntry.COLUMN_LEVEL,"A1");
+        vals.put(DbContract.ClassEntry.COLUMN_EXTRA_INFO,"no extra info");
+        myDataBase.insert(DbContract.ClassEntry.TABLE_NAME,null,vals);
+
+        vals=new ContentValues(4);
+        vals.put(DbContract.ClassContentEntry.COLUMN_BOOK, "no Book");
+        vals.put(DbContract.ClassContentEntry.COLUMN_PAGE, "15-20");
+        vals.put(DbContract.ClassContentEntry.COLUMN_INFO, "no info");
+        vals.put(DbContract.ClassContentEntry.COLUMN_FOREIGN_KEY_CLASS, 1);
+        myDataBase.insert(DbContract.ClassContentEntry.TABLE_NAME,null,vals);
+
+        vals=new ContentValues(4);
+        vals.put(DbContract.StudentEntry.COLUMN_STUDENT_NAME, "Petro");
+        vals.put(DbContract.StudentEntry.COLUMN_EMAIL, "petro@email.com");
+        vals.put(DbContract.StudentEntry.COLUMN_PHONE, "9111234532");
+        vals.put(DbContract.StudentEntry.COLUMN_FOREIGN_KEY_CLASS, 1);
+        myDataBase.insert(DbContract.StudentEntry.TABLE_NAME,null,vals);
+ */
         return null;
     }
 
