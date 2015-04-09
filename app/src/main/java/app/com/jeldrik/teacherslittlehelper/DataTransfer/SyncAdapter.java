bceda0e1c884421e1b02b9216e1be79e45f50d84
jeldrik <jeldriks@gmail.com>
2015-04-09 16:01:22 +0200

    Fixing bugs to make syncadapter workable Testing data insertion from server data into DB with content provider

--- app/src/main/java/app/com/jeldrik/teacherslittlehelper/DataTransfer/SyncAdapter.java
+++ app/src/main/java/app/com/jeldrik/teacherslittlehelper/DataTransfer/SyncAdapter.java
@@ -4,8 +4,10 @@ import android.accounts.Account;
 import android.content.AbstractThreadedSyncAdapter;
 import android.content.ContentProviderClient;
 import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.SyncResult;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.util.JsonReader;
@@ -17,6 +19,8 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+
+import app.com.jeldrik.teacherslittlehelper.data.DbContract;
 
 /**
  * Created by jeldrik on 28/03/15.
@@ -64,27 +68,72 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
     public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
         SyncResult result = new SyncResult();
         try {
+            Log.v(TAG,"syncdimp ");
             getData(provider);
-            Log.v(TAG,"sync");
+            Log.v(TAG,"syncdimp ");
         } catch (RemoteException | IOException e) {
             syncResult.hasHardError();
-            Log.e(TAG,"sync error "+e);
+            Log.e(TAG,"syncdimp error "+e);
         }
     }
     private void getData(ContentProviderClient contentProviderClient) throws RemoteException, IOException{
         URL url=null;
-        try{url =new URL("http","http://192.168.1.153",80,"/teachersLittleHelper/dataSending");}
+        try{url =new URL("http","192.168.1.144",80,"/teachersLittleHelper/dataSending");}
         catch(MalformedURLException e){Log.e(TAG,"Error in the url structure "+e);}
 
         URLConnection conn=url.openConnection();
+        BufferedReader bufReader=new BufferedReader(new InputStreamReader(conn.getInputStream(),"UTF-8"));
+        JsonReader reader=new JsonReader(bufReader);
         try{
-            BufferedReader bufReader=new BufferedReader(new InputStreamReader(conn.getInputStream(),"UTF-8"));
-            JsonReader reader=new JsonReader(bufReader);
-            reader.beginArray();
-            while(reader.hasNext())
-                Log.v(TAG, "JSON "+reader);
-            reader.endArray();
+            reader.beginObject();
+            while(reader.hasNext()) {
+                if (reader.nextName().equals("class")){
+                    reader.beginArray();
+                    while (reader.hasNext()) {
+                        reader.beginObject();
+                        ContentValues vals = new ContentValues(7);
+                        while (reader.hasNext()) {
+                            switch(reader.nextName()){
+                                case "title":
+                                    vals.put(DbContract.ClassEntry.COLUMN_TITLE, reader.nextString());
+                                    break;
+                                case "time":
+                                    vals.put(DbContract.ClassEntry.COLUMN_TIME, reader.nextString());
+                                    break;
+                                case "selectedDays":
+                                    vals.put(DbContract.ClassEntry.COLUMN_DATE, reader.nextString());
+                                    break;
+                                case "endTime":
+                                    vals.put(DbContract.ClassEntry.COLUMN_DURATION, reader.nextString());
+                                    break;
+                                case "location":
+                                    vals.put(DbContract.ClassEntry.COLUMN_LOCATION, reader.nextString());
+                                    break;
+                                case "level":
+                                    vals.put(DbContract.ClassEntry.COLUMN_LEVEL, reader.nextString());
+                                    break;
+                                case "info":
+                                    vals.put(DbContract.ClassEntry.COLUMN_EXTRA_INFO, reader.nextString());
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                        Log.v(TAG, "JSON "+ vals.toString());
+
+                        Uri returnUri = contentProviderClient.insert(DbContract.ClassEntry.CONTENT_URI, vals);
+                        int id=Integer.parseInt(returnUri.getLastPathSegment());
+                        Log.v("MainActivity", "JSON The ID: "+returnUri.getLastPathSegment());
+                        reader.endObject();
+                    }
+                    reader.endArray();
+                }
+            }
+            reader.endObject();
         }catch(IOException e){Log.e(TAG,"Could not read JSON "+e);}
+        finally {
+            reader.close();
+        }
 
     }
 }
--- app/src/main/java/app/com/jeldrik/teacherslittlehelper/MainActivity.java
+++ app/src/main/java/app/com/jeldrik/teacherslittlehelper/MainActivity.java
@@ -4,6 +4,7 @@ import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
@@ -40,6 +41,7 @@ public class MainActivity extends ActionBarActivity implements NewClassFragment.
                     .add(R.id.FragmentContainer, mainFragment)
                     .commit();
         }
+
 
      /*    SQLiteDatabase myDataBase=new DbHelper(this).getReadableDatabase();
 
@@ -129,13 +131,20 @@ public class MainActivity extends ActionBarActivity implements NewClassFragment.
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if(id==R.id.sync){
-            // Create the account type and default account
-            Account newAccount = new Account("dummyaccount", "example.com");
-            AccountManager accountManager = (AccountManager) this.getSystemService(ACCOUNT_SERVICE);
-// If the account already exists no harm is done but
-// a warning will be logged.
-            accountManager.addAccountExplicitly(newAccount, null, null);
-            ContentResolver.requestSync(newAccount,"app.com.jeldrik.teacherslittlehelper.data.ClassContentProvider",Bundle.EMPTY);
+            // Create the dummy account
+            mAccount = CreateSyncAccount(this);
+            // Pass the settings flags by inserting them in a bundle
+            Bundle settingsBundle = new Bundle();
+            settingsBundle.putBoolean(
+                    ContentResolver.SYNC_EXTRAS_MANUAL, true);
+            settingsBundle.putBoolean(
+                    ContentResolver.SYNC_EXTRAS_EXPEDITED, true);
+        /*
+         * Request the sync for the default account, authority, and
+         * manual sync settings
+         */
+            ContentResolver.requestSync(mAccount, DbContract.AUTHORITY, settingsBundle);
+            Toast.makeText(this,"Start syncing",Toast.LENGTH_LONG).show();
         }
 
         return super.onOptionsItemSelected(item);
@@ -241,5 +250,36 @@ public class MainActivity extends ActionBarActivity implements NewClassFragment.
         ClassFragment frag=(ClassFragment)getSupportFragmentManager().findFragmentByTag(ClassFragment.TAG);
         frag.updateMemberVars(title,days,location,startTime,endTime,level,info);
         mainFragment.updateClassinAdapter(days,title,startTime,endTime,id);
+    }
+
+    //---------------------------------------------------------------------------------------------
+    //Authenticator
+    public static final String ACCOUNT_TYPE = "jeldrik.com";
+    // The account name
+    public static final String ACCOUNT = "dummyaccount";
+    // Instance fields
+    Account mAccount;
+    public static Account CreateSyncAccount(Context context){
+        Account newAccount = new Account(ACCOUNT,ACCOUNT_TYPE);
+        AccountManager accountManager=(AccountManager) context.getSystemService(ACCOUNT_SERVICE);
+        /*
+         * Add the account and account type, no password or user data
+         * If successful, return the Account object, otherwise report an error.
+         */
+        if (accountManager.addAccountExplicitly(newAccount, null, null)) {
+            /*
+             * If you don't set android:syncable="true" in
+             * in your <provider> element in the manifest,
+             * then call context.setIsSyncable(account, AUTHORITY, 1)
+             * here.
+             */
+            Toast.makeText(context,"New Account",Toast.LENGTH_LONG).show();
+        } else {
+            /*
+             * The account exists or some other error occurred. Log this, report it,
+             * or handle it internally.
+             */
+        }
+        return newAccount;
     }
 }
